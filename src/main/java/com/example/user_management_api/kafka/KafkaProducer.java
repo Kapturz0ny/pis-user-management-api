cbd39129b356c91b03ce09f2e7dbca559441b33f
= <=>
2025-01-25 18:05:10 +0100

    created kafka producer

---
+++ src/main/java/com/example/user_management_api/kafka/KafkaProducer.java
@@ -0,0 +1,34 @@
+package com.example.user_management_api.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+import com.example.user_management_api.entities.User;
+
+import java.util.Map;
+
+@Service
+public class KafkaProducer {
+
+    private static final String TOPIC = "users-events";
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    @Autowired
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    public void sendMessage(String method, Long id) {
+        LOGGER.info(String.format("Message sent -> method: %s, id: %d", method, id));
+        Map<String, Object> payload = Map.of(
+                "method", method,
+                "id", id
+        );
+        kafkaTemplate.send(TOPIC, payload);
+    }
+}
