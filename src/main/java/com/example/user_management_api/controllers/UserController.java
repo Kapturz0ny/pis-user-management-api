0db5525bd11fa3b8d2fd75660bcb2e4c42277c26
Michal <michal.golawski.mg@gmail.com>
2025-01-26 19:14:58 +0100

    changed uris so gateway api worked properly

--- src/main/java/com/example/user_management_api/controllers/UserController.java
+++ src/main/java/com/example/user_management_api/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users-management/users")
 public class UserController {
 
     private final UserService userService;
